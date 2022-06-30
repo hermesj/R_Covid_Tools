e86fe0cea06fdf4be2fc753581f26ccd34a5253c
Jürgen Hermes <hermesj@uni-koeln.de>
2022-06-30 22:53:00 +0200

    Implemented correlation

--- PlotIncidences.R
+++ PlotIncidences.R
@@ -53,8 +53,6 @@ print("LKs mit Infektionen, ohne Impfungen: ")
 print(relcomp(LK_IDs_Inf,LK_IDs_Vacc))
 print("LKs mit Impfungen, ohne Infektionen: ")
 print(relcomp(LK_IDs_Vacc, LK_IDs_Inf))
-# kreisIDs$name[which(kreisIDs$ags=="16057")]
-# kreisIDs$name[which(kreisIDs$ags=="11000")]
 # kreisIDs$ags[which(kreisIDs$name=="Wartburgkreis")]
 
 
@@ -90,9 +88,10 @@ infPerWeek$Value = infPerWeek$tapply.X...infections.AnzahlFall.837.56..INDEX...i
 
 # Todesfälle pro Woche addieren (alle Kreise)
 deathsPerWeek = data.frame(tapply(X=infections$AnzahlTodesfall, INDEX=infections$week, FUN=sum));
+names(deathsPerWeek)[1] = "Value"
 datesListD <- dates(deathsPerWeek)
 deathsPerWeek$Date = as.Date(datesListD)
-deathsPerWeek$Value = deathsPerWeek$tapply.X...infections.AnzahlTodesfall..INDEX...infections.week../7;
+#deathsPerWeek$Value = deathsPerWeek$tapply.X...infections.AnzahlTodesfall..INDEX...infections.week../7;
 
 # Impfungen pro Woche addieren (alle Kreise)
 vaccsPerWeek = data.frame(tapply(X=vaccinations$Anzahl, INDEX=vaccinations$week, FUN=sum));
@@ -106,18 +105,39 @@ names(vaccsPerLK)[1] <- "Count"
 LKListV <- dates(vaccsPerLK)
 vaccsPerLK$LK = LKListV
 vaccsPerLK = merge(vaccsPerLK, kreisInfos, by.x="LK", by.y="Kennzahl", all=TRUE)
-vaccsPerLK$Incidence <- vaccsPerLK$Einwohner / vaccsPerLK$Count * 100000;
+vaccsPerLK$VaccIncidence <-  vaccsPerLK$Count / vaccsPerLK$Einwohner;
+
+# Todesfälle per LK addieren
+deathsPerLK = data.frame(tapply(X=infections$AnzahlTodesfall, INDEX=infections$IdLandkreis, FUN=sum));
+names(deathsPerLK)[1] = "DeathCount"
+datesListLK <- dates(deathsPerLK)
+deathsPerLK$LK = as.factor(datesListLK)
+deathsPerLK = merge(deathsPerLK, vaccsPerLK)
+deathsPerLK$DeathIncidence <- deathsPerLK$DeathCount / deathsPerLK$Einwohner * 100000
 
 # Plottet ganzen Zeitraum - Impfungen
 plot(x=vaccsPerWeek$Date, y=vaccsPerWeek$Value, ylab="", xlab="", pch=8, type="b", 
      col="darkgreen" ,main ="Impfungen Deutschland")
 
-# Barplot zu Impfinzidenzen auf LK-Ebene
-barplot(Incidence ~ LK, data = vaccsPerLK)
+# Plottet ganzen Zeitraum - Todesfälle
+plot(x=deathsPerLK$Date, y=deathsPerLK$Value, ylab="", xlab="", pch=8, type="b", 
+     col="black" ,main ="Todesfälle Deutschland")
+
+# Barplot zu Impfungen pro EW auf LK-Ebene
+barplot(VaccIncidence ~ LK, data = vaccsPerLK)
+
+# Barplot zu Todesfällen pro 100k EW auf LK-Ebene
+barplot(DeathIncidence ~ LK, data = deathsPerLK)
+
+#Korrelation zwischen Impfungen und Todesfällen (gesamte Pandemie) auf LK-Ebene
+plot(x=deathsPerLK$VaccIncidence, y = deathsPerLK$DeathIncidence, ylab="Todesfälle pro 100k", xlab="Impfungen pro Einwohner")
+abline(lm(deathsPerLK$DeathIncidence~deathsPerLK$VaccIncidence)$coef, col ="red")
 
 # TODO: Ausreißer einsammeln -> Kreisübergreifende Impfungen?
 
-#Plottet ganzen Zeitraum 
+# TODO: Verschiedene Zeitfenster modellieren
+
+#Plottet gesamten Zeitraum 
 plot(x=infPerWeek$Date, y=infPerWeek$Value, ylab="", xlab="", pch=8, type="b", 
      col="red" ,main ="Pandemieverlauf Deutschland Januar 2020 - Mai 2022")
 lines(x=deathsPerWeek$Date, y=deathsPerWeek$Value, ylab="", xlab="", pch=3, 
@@ -163,7 +183,7 @@ lines(x=deathsPerWeek$Date, y=deathsPerWeek$Value, ylab="", xlab="", pch=3,
 legend("topleft",legend=c("Wocheninzidenz","Tägliche Todesfälle in der Woche"), 
        col=c("red","black"), pch=c(8,3),lty=c(1,2), ncol=1)
 
-#Plottet Akt 4
+#Plottet Akt 5
 plot(x=infPerWeek$Date, y=infPerWeek$Value, ylab="", xlab="", pch=8, type="b", 
      col="red", xlim=as.Date(c("2021-11-12","2022-05-25")), ylim=c(0,2000),
      main ="Pandemieverlauf Deutschland November 2021 - Mai 2022")
